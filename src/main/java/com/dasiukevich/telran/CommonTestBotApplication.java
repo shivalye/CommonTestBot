8eb963c891775967c505073e03e0db12decbca0f
Maksim Dasiukevich <dasiukevich@gmail.com>
2024-10-23 09:51:37 +0300

    first commit

---
+++ src/main/java/com/dasiukevich/telran/CommonTestBotApplication.java
@@ -0,0 +1,13 @@
+package com.dasiukevich.telran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonTestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasiukevich/telran/CommonTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasiukevich.telran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
