bdcecb9de26097dd53f87fc3ad9d8b5037f2e599
shivalye <dasiukevich@gmail.com>
2024-10-25 18:24:34 +0300

    basic bot

---
+++ src/main/java/com/dasiukevich/telran/bot/TelegramBot.java
@@ -0,0 +1,73 @@
+package com.dasiukevich.telran.bot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.Level;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import lombok.extern.log4j.Log4j2;
+
+@SuppressWarnings("deprecation")
+@Log4j2
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+	public final String messageHi = "Hi. Can I help you?";
+	@Override
+	public void onUpdateReceived(Update update) {
+//		String message = update.getMessage().getText();
+		String chatId = update.getMessage().getChatId().toString();
+		sendMsg(chatId, messageHi);
+		log.log(Level.ALL, update.toString());
+	}
+
+	@Override
+	public String getBotUsername() {
+		return "CommonTestBot";
+	}
+
+	@Override
+	public String getBotToken() {
+		return "7084928382:AAH1nmJVum_7aruWMhh3xR98TW0nK8CyAIE";
+
+	}
+
+	private synchronized void sendMsg(String chatId, String message) {
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.enableMarkdown(true);
+		sendMessage.setChatId(chatId);
+		sendMessage.setText(message);
+		setButtons(sendMessage);
+		try {
+			execute(sendMessage);
+		} catch (TelegramApiException e) {
+			log.log(Level.TRACE, "Exception: ", e.toString());
+		}
+	}
+	
+	public synchronized void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("Hi"));
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        keyboardSecondRow.add(new KeyboardButton("Help"));
+        KeyboardRow keyboardThirdRow = new KeyboardRow();
+        keyboardThirdRow.add(new KeyboardButton("Contact"));
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardThirdRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+}
